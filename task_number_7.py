5d39ecd0d8cf1f762a598efb430b2629f7c2f916
BOGICAR <85548214+BOGICAR@users.noreply.github.com>
2021-07-05 10:21:33 +0300

    Add files via upload
    Refactoring task №7

--- task_number_7.py
+++ task_number_7.py
@@ -4,15 +4,17 @@ import datetime
 
 print('Enter day, month, year in the format: dd.mm.yyyy')
 input_d_m_y = input('Enter the day: ')
+conv_valid_time = 0
 
 
-def is_date():
+def is_date(conv_valid_time):
     try:
         valid_time = datetime.datetime.strptime(input_d_m_y, '%d.%m.%Y')
         conv_valid_time = bool(valid_time.strftime('%d.%m.%Y'))
+        print(conv_valid_time)
     except:
-        print('False')
-    return conv_valid_time
+        print(bool(conv_valid_time))
+    return
 
 
-print(is_date())
+is_date(conv_valid_time)
